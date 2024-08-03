aa1d39371fb437130d2ee3ea851c8827a474492b
caleb1123 <ryy1507@gmail.com>
2024-08-03 20:27:40 +0700

    first commit

---
+++ src/main/java/com/product/globie/GlobieApplication.java
@@ -0,0 +1,13 @@
+package com.product.globie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/globie/GlobieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.globie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
